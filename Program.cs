9f2786dc49ccff22f37e286bd52b9ba442119523
iyinraphael <iyinraphael@gmail.com>
2025-01-27 13:16:10 -0500

    clean up

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ internal class Program
     private static async Task Main(string[] args)
     {
         // Authenticaiton using Client Credential Oauth token created on Genesys Cloud 
-        var accessTokenInfo = Configuration.Default.ApiClient.PostToken("397af16f-8a2d-4565-9db2-98b3a021cb8e", "kfZIZUtYYMWfL0VCgehpS7UsFbZzptvo9gclpInYItU");
+        var accessTokenInfo = Configuration.Default.ApiClient.PostToken("", "");
         Console.WriteLine("Access token=" + accessTokenInfo.AccessToken);
 
         // Setting purecloud org region 
@@ -21,17 +21,17 @@ internal class Program
 
         // Using Analytics API to to get aggregate conversation details
         var analyticsApi = new AnalyticsApi();
-        // var conversationAnalyticsAggregate = new AnalyticsConversationsAggregates(analyticsApi);
-        // conversationAnalyticsAggregate.PostAnalyticsConversationsAggregates();
+        var conversationAnalyticsAggregate = new AnalyticsConversationsAggregates(analyticsApi);
+        conversationAnalyticsAggregate.PostAnalyticsConversationsAggregates();
 
         // Create Notfication channel
         var notificationsApi = new NotificationsApi();
-        // var notificationChannel = new NotficationChannel(notificationsApi);
-        // var channelUri = notificationChannel.CreateNotficationChannel();
+        var notificationChannel = new NotficationChannel(notificationsApi);
+        var channelUri = notificationChannel.CreateNotficationChannel();
 
         // Subscribe ot converstiona notication for the queue
-        var queueId = "dd5d18cc-a538-4925-ab47-a1cb9b29dd80";
-        var channelId = "streaming-6-61mcndtm5p8nvupq0714ku413d";
+        var queueId = "";
+        var channelId = "";
         var channelTopic = new ChannelTopic 
         {
             Id = $"v2.routing.queues.{queueId}.conversations"
@@ -47,9 +47,9 @@ internal class Program
             Debug.Print("Exception when calling NotificationsApi.PutNotificationsChannelSubscriptions: " + e.Message );
         }
 
-        
+    
         // connect the web socket for live data streaming
-        var websocketClient = new WebSocketClient("wss://streaming.mypurecloud.com/channels/streaming-6-61mcndtm5p8nvupq0714ku413d");
+        var websocketClient = new WebSocketClient("");
         await websocketClient.ConnectToWebsocket();
 
         var queueObservationQuery = new AnalyticsQueryObservation(analyticsApi, queueId);
